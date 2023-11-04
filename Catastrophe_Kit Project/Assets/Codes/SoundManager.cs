0b43efc76cf14c7500e64cf99fedc82410251143
RGButi <orlandogeminiano@gmail.com>
2023-11-04 12:50:05 +0800

    Adding working Sound Button
    November 4, 2023 12:49pm

--- Catastrophe_Kit Project/Assets/Codes/SoundManager.cs
+++ Catastrophe_Kit Project/Assets/Codes/SoundManager.cs
@@ -8,15 +8,26 @@ public class SoundManager : MonoBehaviour
     [SerializeField] Image On;
     [SerializeField] Image Off;
     private bool muted = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        if (!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+        }
+        else
+        {
+            load();
+        }
 
+        Updatepress();
+        AudioListener.pause = true;
     }
 
     public void SoundPress()
     {
-        if(muted == false)
+        if (muted == false)
         {
             muted = true;
             AudioListener.pause = true;
@@ -26,6 +37,31 @@ public class SoundManager : MonoBehaviour
             muted = false;
             AudioListener.pause = false;
         }
+        save();
+        Updatepress();
+    }
+
+    private void Updatepress()
+    {
+        if (muted == false)
+        {
+            On.enabled = true;
+            Off.enabled = false;
+        }
+        else
+        {
+            On.enabled = false;
+            Off.enabled = true;
+        }
+    }
+    private void load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+
+    private void save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
     }
 
 
